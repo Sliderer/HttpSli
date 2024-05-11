7f928e47e9c611cda249d67d3b2d3c6777ee3a1a
Sliderer <slidedru8gs@mail.ru>
2024-05-11 16:41:14 +0300

    add cmake

---
+++ build/CMakeFiles/executable.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/sliderer/projects/HttpSli/src/basic_servers/TCPServer.cpp" "CMakeFiles/executable.dir/src/basic_servers/TCPServer.cpp.o" "gcc" "CMakeFiles/executable.dir/src/basic_servers/TCPServer.cpp.o.d"
+  "/home/sliderer/projects/HttpSli/src/built_servers/HttpServer.cpp" "CMakeFiles/executable.dir/src/built_servers/HttpServer.cpp.o" "gcc" "CMakeFiles/executable.dir/src/built_servers/HttpServer.cpp.o.d"
+  "/home/sliderer/projects/HttpSli/src/enums/RequestType.cpp" "CMakeFiles/executable.dir/src/enums/RequestType.cpp.o" "gcc" "CMakeFiles/executable.dir/src/enums/RequestType.cpp.o.d"
+  "/home/sliderer/projects/HttpSli/src/helpers/http/AddressRouter.cpp" "CMakeFiles/executable.dir/src/helpers/http/AddressRouter.cpp.o" "gcc" "CMakeFiles/executable.dir/src/helpers/http/AddressRouter.cpp.o.d"
+  "/home/sliderer/projects/HttpSli/src/main.cpp" "CMakeFiles/executable.dir/src/main.cpp.o" "gcc" "CMakeFiles/executable.dir/src/main.cpp.o.d"
+  "/home/sliderer/projects/HttpSli/src/models/requests/HttpRequest.cpp" "CMakeFiles/executable.dir/src/models/requests/HttpRequest.cpp.o" "gcc" "CMakeFiles/executable.dir/src/models/requests/HttpRequest.cpp.o.d"
+  "/home/sliderer/projects/HttpSli/src/models/responses/HttpResponse.cpp" "CMakeFiles/executable.dir/src/models/responses/HttpResponse.cpp.o" "gcc" "CMakeFiles/executable.dir/src/models/responses/HttpResponse.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/executable.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/executable.dir/src/basic_servers/TCPServer.cpp.o"
+  "CMakeFiles/executable.dir/src/basic_servers/TCPServer.cpp.o.d"
+  "CMakeFiles/executable.dir/src/built_servers/HttpServer.cpp.o"
+  "CMakeFiles/executable.dir/src/built_servers/HttpServer.cpp.o.d"
+  "CMakeFiles/executable.dir/src/enums/RequestType.cpp.o"
+  "CMakeFiles/executable.dir/src/enums/RequestType.cpp.o.d"
+  "CMakeFiles/executable.dir/src/helpers/http/AddressRouter.cpp.o"
+  "CMakeFiles/executable.dir/src/helpers/http/AddressRouter.cpp.o.d"
+  "CMakeFiles/executable.dir/src/main.cpp.o"
+  "CMakeFiles/executable.dir/src/main.cpp.o.d"
+  "CMakeFiles/executable.dir/src/models/requests/HttpRequest.cpp.o"
+  "CMakeFiles/executable.dir/src/models/requests/HttpRequest.cpp.o.d"
+  "CMakeFiles/executable.dir/src/models/responses/HttpResponse.cpp.o"
+  "CMakeFiles/executable.dir/src/models/responses/HttpResponse.cpp.o.d"
+  "executable"
+  "executable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/executable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
